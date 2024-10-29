ab486a0f10c2dbc77bdc56003ec95c906e0b1451
navaf <navafsoft@gmail.com>
2024-10-29 15:19:11 +0530

    myPow-Mid

--- test.js
+++ test.js
@@ -1,13 +1,22 @@
-var singleNumber = function(nums) {
-    let result = nums.sort((a,b)=>a-b)
-  let finalRes = []
-  for(let i=0;i<result.length;i++){
-      if(result [i] !== result [i+1] ){
-       if(result[i] !== result[i-1])
-           finalRes.push(result[i])
-      }else{
-          i++
-      }
-  }   
-  return finalRes
+// var singleNumber = function(nums) {
+//     let result = nums.sort((a,b)=>a-b)
+//   let finalRes = []
+//   for(let i=0;i<result.length;i++){
+//       if(result [i] !== result [i+1] ){
+//        if(result[i] !== result[i-1])
+//            finalRes.push(result[i])
+//       }else{
+//           i++
+//       }
+//   }   
+//   return finalRes
+// };
+
+var myPow = function(x, n){
+    return Math.pow(x,n)
+    
 };
+let x = 2.00000
+let n = 10
+console.log(myPow(x,n));
+
