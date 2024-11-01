7fc423ac93e6919f6af1e3582633bfe140806045
navaf <navafsoft@gmail.com>
2024-11-01 14:06:00 +0530

    singleNumDuplicate

--- test.js
+++ test.js
@@ -39,13 +39,27 @@
 // console.log(isPowerOfTwo(2));
 
 
-var isPowerOfFour = function(n) {
-    while(n%4==0){
-        n/=4
-    }
-    return n===1
+// var isPowerOfFour = function(n) {
+//     while(n%4==0){
+//         n/=4
+//     }
+//     return n===1
     
+// };
+// console.log(isPowerOfFour(2));
+
+
+
+var singleNonDuplicate = function(nums) {
+    let res=[]
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] !== nums[i+1] && nums[i] !== nums[i-1] ){
+            res.push(nums[i])
+          
+        }
+    }
+    return Number(res)
 };
-console.log(isPowerOfFour(2));
 
-
+nums = [1,1,2,3,3,4,4,8,8]
+console.log(singleNonDuplicate(nums));
